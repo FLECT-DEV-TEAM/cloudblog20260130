6d3d18ad95ddabc227e8207d42e68bf9ed3dbc89
Masashi Sato <masashi.sato@flect.co.jp>
2026-01-30 13:47:09 +0900

    remove duplication

--- force-app/main/default/lwc/menuChoiceRenderer/menuChoiceRenderer.js
+++ force-app/main/default/lwc/menuChoiceRenderer/menuChoiceRenderer.js
@@ -11,29 +11,8 @@ export default class MenuChoiceRenderer extends LightningElement {
     return this.value?.items || [];
   }
 
-  get utilAPI() {
-    return (
-      window.embeddedservice_configuration?.util ||
-      window.parent?.embeddedservice_configuration?.util ||
-      null
-    );
-  }
-
   handlePick(event) {
     const choice = event.currentTarget.dataset.choice;
-
-    const api = this.utilAPI;
-    if (!api?.sendTextMessage) {
-      console.warn('Enhanced Web Chat utilAPI.sendTextMessage is not available yet.');
-      return;
-    }
-
-    api.sendTextMessage(choice)
-      .then(() => {
-        console.log(`sendTextMessage succeeded: ${choice} `);
-      })
-      .catch((e) => {
-        console.error('sendTextMessage failed', e);
-      });
+    window.parent.sendMessage(choice);
   }
 }
